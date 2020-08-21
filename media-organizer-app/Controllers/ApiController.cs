fb7ef4f9aa15ed1f39e5f42ede10286152a5a161
David Hatfield <dhatfield@at-pw.com>
2020-08-21 15:35:16 -0700

    Add project files.

---
+++ media-organizer-app/Controllers/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace media_organizer_app.Controllers {
+	[Route("[controller]")]
+	[ApiController]
+	public class ApiController : ControllerBase {
+		[HttpGet]
+		public string Get() {
+			return "yo yo yo";
+		}
+	}
+}
---
+++ media-organizer-app/Services/DataManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace media_organizer_app.Services {
+	public class DataManager {
+
+	}
+
+	public class FileIndexItem {
+		public string path { get; set; }
+		public string type { get; set; }
+		public List<object> metadata { get; set; }
+	}
+}
